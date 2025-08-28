278feb884003302f85d6b5f6cd0448a5071f5598
YJRenK9 <jordan.quan.le@gmail.com>
2025-08-28 11:45:55 -1000

    updated simplebase a bit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Your code here will run after the DOM is fully loaded
+    console.log("Document is ready!");
+
+    // global functions go here
+    this.style.backgroundColor = "aqua";
+});

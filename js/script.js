dfe854f4af03fd8cc086a00c105053ad7295a154
YJRenK9 <jordan.quan.le@gmail.com>
2025-09-30 17:36:43 -1000

    Finished implementing the mobile menu options animation for now

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Your code here will run after the DOM is fully loaded
-    console.log("Document is ready!");
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Your code here will run after the DOM is fully loaded
+//     console.log("Document is ready!");
 
-    // global functions go here
-    this.style.backgroundColor = "aqua";
+//     // global functions go here
+//     this.style.backgroundColor = "aqua";
+// });
+const siteNav = document.querySelector('.site-nav');
+const menuButton = document.querySelector('.menu-button');
+
+
+menuButton.addEventListener('click', () => {
+    const currentState = siteNav.getAttribute('data-menustate');
+
+    if (siteNav.getAttribute('data-menustate') === 'closed') {
+        siteNav.setAttribute('data-menustate', 'open');
+    } else {
+        siteNav.setAttribute('data-menustate', 'closed');
+    }
+
+    console.log("Ay you got any ramen yo!");
+    console.log(siteNav.getAttribute('data-menustate'));
 });
